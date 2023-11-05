5d1788c77055bf315c985beedfd2dcaf3656ff3e
hzionn <zionn@zionn.local>
2023-11-05 15:57:07 +0800

    update lvim configs

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -5,9 +5,32 @@
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
 
--- Settings
-lvim.colorscheme = "carbonfox"
+-- Plugins
+lvim.plugins = {
+    -- colorschemes
+    { "lunarvim/colorschemes" },
+    { "Mofiqul/vscode.nvim" },
+    { "sainnhe/gruvbox-material" },
+    { "sainnhe/everforest" },
+    { "sainnhe/edge" },
+    { "rebelot/kanagawa.nvim" },
+    { "EdenEast/nightfox.nvim" },
+}
 
+-- Vim Options
+vim.opt.showmode = true -- this will show current vim mode
+vim.opt.wrap = true
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- insert n spaces for a tab
+vim.opt.scrolloff = 15
+vim.opt.relativenumber = true
+vim.opt.timeoutlen = 500 -- time to wait for a mapped sequence to complete (in milliseconds)
+
+-- Settings
+lvim.colorscheme = "everforest" -- edge, aurora, everforest, gruvbox-material
+lvim.transparent_window = true
+
+lvim.builtin.which_key.setup.window.winblend = 20 -- value between 0-100 0 for fully opaque and 100 for fully transparent
 lvim.builtin.which_key.mappings["t"] = {
     name = "+Terminal",
     f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
@@ -32,32 +55,4 @@ lvim.builtin.alpha.dashboard.section.header.val = {
     "╚██████╗╚██████╔╝██████╔╝██║██║ ╚████║╚██████╔╝",
     " ╚═════╝ ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝ ",
 }
-
--- Plugins
-lvim.plugins = {
-    -- colorschemes
-    { "lunarvim/colorschemes" },
-    { "Mofiqul/vscode.nvim" },
-    { "sainnhe/gruvbox-material" },
-    { "sainnhe/everforest" },
-    { "sainnhe/edge" },
-    { "rebelot/kanagawa.nvim" },
-    { "EdenEast/nightfox.nvim" },
-    -- others
-    -- {
-    --   "stevearc/dressing.nvim",
-    --   config = function()
-    --     require("dressing").setup({
-    --       input = { enabled = false },
-    --     })
-    --   end,
-    -- },
-}
-
--- Vim Options
-vim.opt.showmode = true -- this will show current vim mode
-vim.opt.wrap = true
-vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 4 -- insert 2 spaces for a tab
-vim.opt.scrolloff = 15
 
